881d6e46dad0eb74ce50565bdd3eecc30f558d81
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-04 17:00:30 +0200

    creating components ans service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { RepositoryFormComponent } from './repository-form/repository-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent,
+    RepositoryFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/repository-form/repository-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-form',
+  templateUrl: './repository-form.component.html',
+  styleUrls: ['./repository-form.component.css']
+})
+export class RepositoryFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
