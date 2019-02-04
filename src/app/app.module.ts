2eda3d9d47e9ab109d16b23cbd1ab1157e4ea5cc
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-04 17:22:54 +0200

    updating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RepositoryService } from './repository.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositoryComponent } from './repository/repository.component';
 import { RepositoryFormComponent } from './repository-form/repository-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RepositoryComponent,
-    RepositoryFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, RepositoryComponent, RepositoryFormComponent ],
+	imports: [ BrowserModule, HttpClientModule ],
+	providers: [ RepositoryService ],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
