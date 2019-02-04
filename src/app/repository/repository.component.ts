edaa49d73c57ecf539d33cd1c31e37a0d2bf2303
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-04 19:16:29 +0200

    fixing errors

--- src/app/repository/repository.component.ts
+++ src/app/repository/repository.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { RepositoryService } from '../repository.service';
+import { User } from '../user';
 
 @Component({
-  selector: 'app-repository',
-  templateUrl: './repository.component.html',
-  styleUrls: ['./repository.component.css']
+	selector: 'app-repository',
+	templateUrl: './repository.component.html',
+	styleUrls: [ './repository.component.css' ],
+	providers: [ RepositoryService ]
 })
 export class RepositoryComponent implements OnInit {
+	user: User;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor(private repositoryService: 'RepositoryService', public repoService: 'RepositoryService') {
+		//  this.user = this.repositoryService.user;...work of tomorrow start on this
+	}
+	ngOnInit() {
+		// this.user = this.repositoryService.user;
+	}
 }
