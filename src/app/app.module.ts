3fee6c073a6989cf41a2f422ea1713cf04d66389
Jeanne d'Arc NYIRAMWIZA <njoanc@gmail.com>
2019-02-05 14:11:14 +0200

    route configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { RepositoryComponent } from './repository/repository.component';
 import { RepositoryFormComponent } from './repository-form/repository-form.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-// import { RouterModule, Routes } from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
+// Defining routes
+const routes: Routes = [
+	{ path: 'users', component: RepositoryComponent }
+	// {path:"about",component:AboutComponent}
+];
 @NgModule({
 	declarations: [ AppComponent, RepositoryComponent, RepositoryFormComponent, NotFoundComponent ],
-	imports: [ BrowserModule, HttpClientModule ],
+	imports: [ BrowserModule, HttpClientModule, RouterModule.forRoot(routes) ],
 	// providers: [ RepositoryService ],
 	bootstrap: [ AppComponent ]
 })
